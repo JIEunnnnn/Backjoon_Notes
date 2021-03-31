433b641686387f2c8283a40aeb8e5267e3fb1011
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2021-03-31 17:12:55 +0900

    Update 2048.py

--- 삼성 SW 역량테스트/2048.py
+++ 삼성 SW 역량테스트/2048.py
@@ -2,7 +2,140 @@
 #주어진 입력값에 따라 상하좌우값 더해서 최대값 구하는 알고리즘 
 #
 #
-#
+#https://juhee-maeng.tistory.com/90
+
+import copy 
+
+def find_max(array) :
+  global maxv 
+  for i in range(N):
+    for j in range(N):
+      if maxv < array[i][j] :
+        maxv = array[i][j]
+
+def move_left(board) :
+  for i in range(N) :
+    p = 0
+    x = 0
+    for q in range(N) :
+     
+      if board[i][q] == 0 : #0이면 건너뛰기
+        continue 
+      if x == 0 :
+        x = board[i][q]
+      else :
+        if x == board[i][q] :
+          board[i][p] = 2 * x
+          p = p+1 
+          x = 0
+        else :
+          # q가 1이라면 p 는 0번째 자리 
+          board[i][p] = x 
+          p = p + 1 
+          x = board[i][q] 
+
+      board[i][q] = 0 #블록이동했으므로 0으로 초기화
+
+    if x != 0 :
+      board[i][p] = x 
+  #print(board)
+  return board
+
+def move_right(board) :
+  for i in range(N) :
+    p = N -1 
+    x = 0
+    for q in range(N-1, -1, -1):
+      if board[i][q] == 0 :
+        continue 
+      if x == 0 :
+        x = board[i][q] 
+      else :
+        if x == board[i][q] :
+          board[i][p] = 2 * x
+          p = p-1 
+          x = 0
+        else :
+          board[i][p] = x 
+          p = p - 1 
+          x = board[i][q] 
+      
+      board[i][q] = 0 
+
+    if x != 0 :
+        board[i][p] = x 
+  print(board)
+  return board
+
+def move_up(board) :
+  for i in range(N) :
+      p = 0
+      x = 0
+      for q in range(N) :
+        if board[q][i] == 0:
+          continue 
+        if x == 0 :
+          x = board[q][i]
+        else :
+          if x == board[q][i] :
+            board[p][i] = 2* x
+            p = p +1
+            x =0
+          else :
+            board[p][i] = x 
+            p = p+1
+            x = board[q][i]
+        board[q][i] = 0
+      if x != 0 :
+        board[p][i] = x 
+  
+  #print(board)
+  return board 
+
+def move_down(board) :
+  for i in range(N) :
+    p = N-1
+    x = 0
+    for q in range(N-1, -1, -1):
+      if board[q][i] == 0 :
+        continue 
+      if x == 0 :
+        x = board[q][i]
+      else :
+        if x == board[q][i] :
+          board[p][i] = 2 * x
+          p = p -1
+          x = 0
+        else :
+          board[p][i] = x 
+          p = p-1 
+          x = board[q][i]
+      board[q][i] = 0
+    if x != 0 :
+      board[p][i] = x
+  
+  #print(board)
+  return board 
+
+def dfs(dfs_board, n) :
+  if n == 5 :
+    find_max(dfs_board)
+    return 
+  dfs(move_left(copy.deepcopy(dfs_board)), n+1)
+  dfs(move_right(copy.deepcopy(dfs_board)), n+1)
+  dfs(move_up(copy.deepcopy(dfs_board)), n+1)
+  dfs(move_down(copy.deepcopy(dfs_board)), n+1)
+
+
+
+
+if __name__ == '__main__':
+  N = int(input())
+  maxv = 0
+  map_board = [list(map(int, input().split())) for _ in range(N)]
+  dfs(map_board, 0)
+  print(maxv)
+
 
 #1차시도 => 실패
 from sys import stdin
