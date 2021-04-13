4ce564638a310f48215efea9e68c04e71919bbf9
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2021-04-13 19:15:17 +0900

    Update 투에-모스 문자열.py

--- Silver/투에-모스 문자열.py
+++ Silver/투에-모스 문자열.py
@@ -4,6 +4,26 @@
 #
 #
 
+#2차시도 시간초과 
+from sys import stdin
+
+n = int(stdin.readline()) 
+
+answer = '01'
+tmp = 2 
+while True :
+  for i in range(tmp) :
+    if answer[i] == '0' :
+      answer += '1'
+    else :
+      answer += '0'
+
+  if tmp < n :
+      tmp = tmp ** 2
+  else :
+    print(answer[n-1])
+    break
+
 #1차시도 시간초과
 from sys import stdin
 import math
