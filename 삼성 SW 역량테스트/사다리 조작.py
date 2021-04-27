9bee4bfbb8f71e2545e5829633809531452b35f7
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2021-04-27 18:51:36 +0900

    Update 사다리 조작.py

--- 삼성 SW 역량테스트/사다리 조작.py
+++ 삼성 SW 역량테스트/사다리 조작.py
@@ -1,13 +1,74 @@
+#백준15684 사다리 조작
+#최대 3개의 사다리 조작해서, i번째에 i 가 출력되는 사다리 게임
+#
+#1차시도 때, 가로-세로 로 구현했는데 이게 많이 헷갈렸다. + 조합X dfs로 풀기!
+#다른 풀이 참고하니까 maps[세로][가로]로 구현함(유동적으로 풀자!)
+#https://chldkato.tistory.com/151
+
+import sys
+
+input = sys.stdin.readline
+n,m,h = map(int, input().split()) #세로, 연결할 가로수, 가로
+maps = [[0]*h for _ in range(n)] #maps[세로][가로]
+
+def move() :
+    #검사하는 메소드
+    for i in range(n) :
+        num = i
+        for j in range(h) :
+            if maps[num][j] :
+                num +=1
+            elif maps[num-1][j] :
+                num -= 1
+        if i != num :
+            return 0
+    return 1
+
+#어느 가로선을 연결할 것인가?
+def dfs(cnt, idx, r) :
+    #print(maps)
+    global ans
+    if cnt == r :
+        if move() == 1 :
+            ans = cnt
+        return
+
+    for i in range(idx, h) : #가로
+        for j in range(n-1) : #세로
+            if maps[j][i] : #maps[j][i] = 1 일경우
+                continue
+            if j-1 >= 0 and maps[j-1][i]:
+                continue
+            if j + 1 < n and maps[j + 1][i]:
+                continue
+
+            maps[j][i] = 1
+            dfs(cnt + 1, i, r)
+            maps[j][i] = 0
 
 
 
+for _ in range(m) :
+    x, y = map(int, input().split()) #가로선, 세로선
+    maps[y-1][x-1] = 1 #연결관계 저장
 
+#print(maps)
+ans, flag =sys.maxsize, 1
+for r in range(4) :
+    dfs(0,0,r) #최대3개의 가로선추가,,,
+    if ans != sys.maxsize:
+        print(ans)
+        flag = 0
+        break
+
+if flag == 1 :
+    print(-1)
+
+======================================================================
 #1차시도.. => 테케도 통과못함ㅠ 뭐가문제지.. 
 from sys import stdin
 from copy import deepcopy
 from itertools import combinations
-import math
-from collections import  deque
 
 dxs = [(0,1),(0,-1)]
 n,m,h = map(int, stdin.readline().split())
